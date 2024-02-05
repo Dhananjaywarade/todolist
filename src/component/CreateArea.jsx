75d61698529f7dfa1e6a914ba4e4711eb334d786
Dhananjay <waradedhananjay2000@gmail.com>
2024-02-05 10:49:34 +0530

    create a function handlechange

--- src/component/CreateArea.jsx
+++ src/component/CreateArea.jsx
@@ -5,12 +5,23 @@ const CreateArea = () => {
         title:"",
         content:""
     });
-    
+
+    function handleChange(event) {
+        const {name,value}=event.target;
+
+        setNote(prevNote=>{
+            return{
+                ...prevNote,
+                [name]:value
+            };
+        })
+    }
+
   return (
     <div>
         <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
+        <input name="title" value={note.title} onChange={} placeholder="Title" />
+        <textarea name="content" value={note.content} onChange={} placeholder="Take a note..." rows="3" />
         <button>Add</button>
       </form>
     </div>
